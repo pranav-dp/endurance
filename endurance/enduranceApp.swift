fd6bec7903cd238bc7e3836227232b368772acbb
Pranav <pranav.duraisaamy@gmail.com>
2026-01-29 12:41:56 +0530

    Initial Commit

---
+++ endurance/enduranceApp.swift
@@ -0,0 +1,32 @@
+//
+//  enduranceApp.swift
+//  endurance
+//
+//  Created by Pranav on 29/01/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct enduranceApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
